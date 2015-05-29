f252740f09a58d8ba6ea38363da20a733bd979e7
JMC11 <JMC11614792@gmail.com>
2015-05-29 16:22:16 -0400

    choice controller route skeleton

---
+++ app/controllers/choices.rb
@@ -0,0 +1,23 @@
+get '/surveys/:id/questions/:question_id/choices/new' do
+
+end
+
+post '/surveys/:id/questions/:question_id/choices/' do
+
+end
+
+get '/surveys/:id/questions/:question_id/choices/choice_id' do
+
+end
+
+get '/surveys/:id/questions/:question_id/choices/choice_id/edit' do
+
+end
+
+put '/surveys/:id/questions/:question_id/choices' do
+
+end
+
+delete '/surveys/:id/questions/:question_id/choices' do
+
+end
