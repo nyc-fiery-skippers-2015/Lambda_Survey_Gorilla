2cab50d3a8560486894fdbda64b9e1fbcf80e3dc
JMC11 <JMC11614792@gmail.com>
2015-05-29 16:49:55 -0400

    choice routes

--- app/controllers/choices.rb
+++ app/controllers/choices.rb
@@ -1,23 +1,32 @@
 get '/surveys/:id/questions/:question_id/choices/new' do
-
+  erb :'/choices/new'
 end
 
 post '/surveys/:id/questions/:question_id/choices/' do
-
+  new_choice = Choice.new(params[:choice])
+  return [500, 'Invalid Choice'] unless current_choice.save
+  redirect '/surveys/:id/questions/:question_id/choices/#{new_choice.id}'
 end
 
 get '/surveys/:id/questions/:question_id/choices/choice_id' do
-
+  current_choice = Choice.find_by(id: params[:id])
+  erb :'/choices/show' locals: {choice: current_choice}
 end
 
 get '/surveys/:id/questions/:question_id/choices/choice_id/edit' do
-
+  current_choice = Choice.find_by(id: params[:id])
+  erb :'/choices/edit', locals: {choice: current_choice}
 end
 
 put '/surveys/:id/questions/:question_id/choices' do
-
+  current_choice = Choice.find_by(id: params[:id])
+  return [500, 'Invalid Choice'] unless current_choice
+  current_choice.update(params[:choice])
+  redirect '/surveys/:id/questions/:question_id/choices/:choice_id'
 end
 
 delete '/surveys/:id/questions/:question_id/choices' do
-
+  current_choice = Choice.find_by(id: params[:id])
+  current_choice.destroy
+  redirect '/surveys/:id/questions/:question_id'
 end
